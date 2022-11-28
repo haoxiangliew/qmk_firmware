366e9530b601af89c90051919e75bf0572d4780d
Hao Xiang Liew <haoxiangliew@gmail.com>
2022-11-28 09:34:24 -0500

    Update miryoku

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,6 @@
-# Copyright 2019 Manna Harbour
+# Copyright 2022 Manna Harbour, haoxiangliew
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS = QWERTY
+LTO_ENABLE = yes
+DEBOUNCE_TYPE = sym_eager_pk
